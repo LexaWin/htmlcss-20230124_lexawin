9d85a25ededf306d9c420d3837ad07f0b5fcee4a
Aleksei Vinter <lexawin@gmail.com>
2023-03-19 20:37:00 +0300

    deploy on gh-pages

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  syntax: 'postcss-scss',
+  map: { inline: true },
+  plugins: [
+    require('postcss-import'),
+    require('postcss-advanced-variables'),
+    require('postcss-nested'),
+  ],
+};
